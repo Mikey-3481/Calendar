e3fd34f24185db4c037a18cf0fde91f1a021b274
Michael Walsh <michaelislikethegod@example.com>
2024-12-12 15:34:50 -0500

    Implemented the functionality to get the number of days from 2000/1/1 to selected month and find the find the first weekday of the selected month

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,8 +3,29 @@ import React, { useState } from "react";
 export default function Board() {
   const [selectedYear, setSelectedYear] = useState(2000);
   const [selectedMonth, setSelectedMonth] = useState(1);
-  const Dates = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-  let lastDays = 0;
+  const days = [];
+  const dates = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  const isLeapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+  };
+
+  const setLastDays = (year, month) => {
+    let totalDays = 0;
+
+    for (let y = 2000; y < year; y++) {
+      totalDays += isLeapYear(y) ? 366 : 365;
+    }
+
+    for (let m = 0; m < month - 1; m++) {
+      totalDays += months[m];
+    }
+    
+    if (isLeapYear(year)) totalDays += 1;
+
+    return  totalDays;
+  }
 
   const yearArr = [];
   const monthArr = [];
@@ -17,7 +38,9 @@ export default function Board() {
     <div className="board">
       <header>
         <label htmlFor="year-select">Year :</label>
-        <select name="" id="year-select"
+        <select
+          name=""
+          id="year-select"
           onChange={(e) => setSelectedYear(Number(e.target.value))}
         >
           {yearArr.map((year) => (
@@ -27,7 +50,9 @@ export default function Board() {
           ))}
         </select>
         <label htmlFor="month-select">Month :</label>
-        <select name="" id="month-select"
+        <select
+          name=""
+          id="month-select"
           onChange={(e) => setSelectedMonth(Number(e.target.value))}
         >
           {monthArr.map((month) => (
@@ -38,11 +63,12 @@ export default function Board() {
         </select>
       </header>
       <body>
-        {Dates.map((date) => (
+        {dates.map((date) => (
           <date key={date}>
             <b>{date}</b>
           </date>
         ))}
+        {}
       </body>
     </div>
   );
